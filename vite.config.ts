42ec0fe72893eecf2b1053f244534caabf0d0c0b
Ujjawal-Rajput <rujjawal162gmail.com>
2025-01-17 16:43:33 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ghPages(), // Add the GitHub Pages plugin
+  ],
   base:"/SummarEase-frontend/",
   optimizeDeps: {
     exclude: ['lucide-react'],
