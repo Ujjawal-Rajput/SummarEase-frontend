38cbf39bb20f0356daa16f05e8299f2afe76e3b9
Ujjawal-Rajput <rujjawal162gmail.com>
2025-01-18 15:22:50 +0530

    change navigate to Navigate

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { authState } from '../Store/State';
 
 const ProtectedRoute = ({ children }) => {
   const auth = useRecoilValue(authState);
-  const navigate = useNavigate();
   console.log(auth)
 
-  return auth.user ? children : navigate("/login");
+  return auth.user ? children : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
